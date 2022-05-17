3f4048b4ee0e4dbd1c42888950060abc92192809
Linh Huynh <70009294+linh2020@users.noreply.github.com>
2022-05-17 03:24:56 -0700

    5172022-004

---
+++ JavaProject/src/Index.java
@@ -0,0 +1,15 @@
+public class Index {
+
+    public static void main(String[] args) {
+        String notification = "Index";
+        String fullName = "David Huynh";
+        String bookName = "Java Core";
+
+        bookName = "C++";
+
+        System.out.println("Notification: " + notification);
+        System.out.println("Full Name: " + fullName);
+        System.out.println("Book Name: " + bookName);
+
+    }
+}
